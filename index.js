c22432a2f2f96286d7ef3b39b52ab0232916fa23
Rafael Paiva <r.rafapaiva@gmail.com>
2020-01-28 15:59:52 +1000

    Added routes

--- index.js
+++ index.js
@@ -3,10 +3,46 @@ const server = express();
 
 server.use(express.json());
 
+const projects = [{"id": "1", "title": "GoStack", "tasks": []}];
 
+server.get('/projects', (req, res) => {
+    res.json(projects);
+});
 
-server.get('/', (req, res) => {
-    res.json({ name: 'Diego'});
+server.post('/projects', (req, res) =>{
+    const {id, title} = req.body;
+    projects.push({id:`${id}`, title:`${title}`, tasks: []});
+    return res.json(projects);
+
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    projects[id].tasks.push(title);
+
+    return res.json(projects);
+});
+
+server.put('/projects/:id', (req, res) => {
+
+    const id = Number(req.params.id);
+    const { title } = req.body;
+
+    projects[id].title = title;
+
+    return res.json(projects[id]);
+});
+
+server.delete('/projects/:id', (req, res) => {
+    
+    const id = Number(req.params.id);
+    const project = projects[id].title;
+    
+    projects.splice(id, 1);
+
+    return res.json(`Project ${project} was deleted`);
 
 });
 
